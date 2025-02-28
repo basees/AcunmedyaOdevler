4151b30e4aa8ae03408e28616cded4b6dc0f6fc5
BEYZA <fatmacandemir3@gmail.com>
2025-02-28 21:07:23 +0300

    Odev 1 yapildi.

---
+++ ders1/ders1/Classes/car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders1.Classes
+{
+    public class car
+    {
+        public string marka;
+        public string vites;
+     public void caryaz()
+        {
+            Console.WriteLine(marka + "->" + vites + " viteslidir.");
+        }
+    }
+}
---
+++ ders1/ders1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ders1.Classes;
+
+car toy1 = new car();
+toy1.marka = "BMW";
+toy1.vites = "Düz";
+toy1.caryaz();
+
+car toy2 = new car();
+toy2.marka = "Porsche";
+toy2.vites = "Otomatik";
+toy2.caryaz();
+
+car toy3 = new car();
+toy3.marka = "TOGG";
+toy3.vites = "Otomatik";
+toy3.caryaz();
+
+car toy4 = new car();
+toy4.marka = "Audi";
+toy4.vites = "Düz";
+toy4.caryaz();
+
+car toy5 = new car();
+toy5.marka = "Toyota";
+toy5.vites = "Düz";
+toy5.caryaz();
